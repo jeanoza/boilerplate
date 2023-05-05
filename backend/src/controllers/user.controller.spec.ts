553a8409a2e39c5f1dfb0017dde598d837b02311
jeanoza <choikb916@gmail.com>
2023-05-05 13:52:11 +0200

    fix:inutil line

--- backend/src/controllers/user.controller.spec.ts
+++ backend/src/controllers/user.controller.spec.ts
@@ -50,7 +50,6 @@ describe("UserController", () => {
   describe("findAll", () => {
     it("should return all user if there is at least one user in db", async () => {
       jest.spyOn(userService, "findAll").mockResolvedValueOnce(users);
-      // res.json = jest.fn<any>().mockReturnValueOnce(res);
 
       await userController.findAll(req, res);
 
@@ -73,7 +72,6 @@ describe("UserController", () => {
       req = { params: { id: 1 } } as unknown as Request;
 
       jest.spyOn(userService, "findById").mockResolvedValueOnce(user);
-      // res.json = jest.fn<any>().mockReturnValueOnce(res);
 
       await userController.findById(req, res);
 
@@ -86,8 +84,6 @@ describe("UserController", () => {
       jest
         .spyOn(userService, "findById")
         .mockRejectedValueOnce("User not found");
-      // res.status = jest.fn<any>().mockReturnValueOnce(res);
-      // res.json = jest.fn<any>().mockReturnValueOnce(res);
 
       await userController.findById(req, res);
 
@@ -108,9 +104,6 @@ describe("UserController", () => {
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith(user);
     });
-  });
-
-  describe("findByEmail", () => {
     it("should return an error if no user corresponding to email", async () => {
       req = { params: { email: "john@gmail.com" } } as unknown as Request;
       jest
