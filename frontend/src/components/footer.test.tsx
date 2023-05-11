24b16f876e33bbae60cb8d4989526967d791760a
jeanoza <choikb916@gmail.com>
2023-05-11 13:27:40 +0200

    test(footer)

---
+++ frontend/src/components/footer.test.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./footer"
+import { render, screen } from '@testing-library/react';
+
+
+describe('<Footer/>', () => {
+	beforeEach(() => {
+		// eslint-disable-next-line testing-library/no-render-in-setup
+		render(<Footer />, { wrapper: BrowserRouter });
+	})
+	it('render without error', () => {
+		const footer = screen.getByTestId('footer');
+		expect(footer).toBeInTheDocument();
+	})
+	it('has a github link and redirect to my github', () => {
+		const githubLink = screen.getByTestId('github-link');
+		expect(githubLink).toBeInTheDocument();
+		expect(githubLink).toHaveAttribute('href', 'https://www.github.com/jeanoza')
+	})
+	it('has a linkedin link and redirect to my linkedin', () => {
+		const linkedinLink = screen.getByTestId('linkedin-link');
+		expect(linkedinLink).toBeInTheDocument();
+		expect(linkedinLink).toHaveAttribute('href', 'https://www.linkedin.com/in/kyubong-choi-489198176/')
+	})
+})
